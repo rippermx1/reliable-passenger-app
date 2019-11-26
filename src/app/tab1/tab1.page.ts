269c6becfc2c0793627dff7fd4d470c2699cc06a
Carlos Valdes <carlosv@simplemoviles.com>
2019-11-26 15:47:10 -0300

    new/header title & tab order

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import { PassengerService } from '../services/passenger.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,21 +6,7 @@ import { PassengerService } from '../services/passenger.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
-  passengersList: any;
-  constructor(private passengerService: PassengerService) {}
+  constructor() {}
   ngOnInit(): void {
-    /*this.passengerService.passengers().subscribe(
-        value => {
-          console.log(value);
-        },
-        error1 => {
-          console.error(error1);
-        },
-        () => {
-          console.log('passengerService.passengers() => Complete');
-        }
-    );*/
-    this.passengersList = this.passengerService.passengers();
-    console.log(this.passengersList);
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { PassengerService } from '../services/passenger.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
-  constructor() {}
-
+export class Tab2Page implements OnInit {
+  passengersList: any;
+  constructor(private passengerService: PassengerService) {}
+  ngOnInit(): void {
+    this.passengersList = this.passengerService.passengers();
+    console.log(this.passengersList);
+  }
 }
