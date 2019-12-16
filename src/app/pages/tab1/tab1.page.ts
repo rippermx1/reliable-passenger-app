af79ee351c1015e54bf7674e2cde40e6397756b5
carlos valdés <valdescarlos17@gmail.com>
2019-12-16 01:02:21 -0300

    modify/tab1 mockup, reject functionality

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import random from '@angular-devkit/schematics/src/rules/random';
 
 @Component({
   selector: 'app-tab1',
@@ -6,10 +7,59 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  /**
+   * Navbar values
+   */
   icon: string = 'logo-model-s';
   url: string = '/my-car';
   title: string = 'Viajes';
+
+  /**
+   * Fingerprint images
+   */
+  fingerprints: any[] = [
+    '../assets/images/finger1.jpeg',
+    '../assets/images/finger2.png',
+    '../assets/images/finger3.png'
+  ];
+  fingerprintSelected: string;
+
+  /**
+   * Set a random fingerprint image
+   */
+  randomFingerprint() {
+    this.fingerprintSelected = this.fingerprints[Math.floor(Math.random() * 3)];
+  }
+
+  class: string;
+
+  /**
+   * Set visible class
+   */
+  setVisibleClass(state: boolean) {
+    if (!state)
+      this.class = "reject-passenger";
+    else
+      this.class = "accept-passenger";
+  }
+
   constructor() {}
   ngOnInit(): void {
+    this.randomFingerprint();
+    this.setVisibleClass(true);
+  }
+
+  /**
+   * Accept passenger
+   */
+  acceptPassenger() {
+
+  }
+
+  /**
+   * Reject passenger
+   */
+  rejectPassenger() {
+    this.setVisibleClass(false);
   }
 }
