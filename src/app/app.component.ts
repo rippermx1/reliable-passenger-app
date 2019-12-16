c7c619c884776e3428e5c8d67528d7a8176329b7
Carlos Valdes <carlosv@simplemoviles.com>
2019-12-16 17:45:29 -0300

    new/side menu thumbnail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  thumbnail: string = './assets/images/car.svg';
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
